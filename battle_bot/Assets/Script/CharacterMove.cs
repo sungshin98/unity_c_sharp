f6769d6814f1b08ea9a6295937d992cbbf461b0b
sungshin98 <130137059+sungshin98@users.noreply.github.com>
2023-11-08 13:55:24 +0900

    Screen segmentation print input key

--- battle_bot/Assets/Script/CharacterMove.cs
+++ battle_bot/Assets/Script/CharacterMove.cs
@@ -1,6 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterMove : MonoBehaviour
 {
@@ -11,15 +10,62 @@ public class CharacterMove : MonoBehaviour
     public float gravity = -20f;
     public float yVelocity = 0;
 
+    [SerializeField]
+    private int playerNumber = 1; // Serialize 필드를 사용하여 플레이어 번호를 설정합니다.
+    public Text keyText;
+    private string playerPrefix = "Player ";
     void Start()
     {
-
+        keyText.text = "Key Input: ";
     }
 
     void Update()
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
+        float h = 0;
+        float v = 0;
+        if (playerNumber == 1 || playerNumber == 2)
+        {
+            keyText.text = "Key Input: " + GetKeyInput(); // GetKeyInput 함수를 호출하여 입력된 키를 표시합니다.
+        }
+        if (playerNumber == 2)
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                h = -1; // 왼쪽 방향키 입력
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                h = 1; // 오른쪽 방향키 입력
+            }
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                v = 1; // 위쪽 방향키 입력
+            }
+            else if (Input.GetKey(KeyCode.DownArrow))
+            {
+                v = -1; // 아래쪽 방향키 입력
+            }
+        }
+        else if (playerNumber == 1)
+        {
+            // 2플레이어의 방향 키 입력 처리 (키 코드를 수정하여 원하는 키를 사용할 수 있습니다)
+            if (Input.GetKey(KeyCode.A))
+            {
+                h = -1;
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                h = 1;
+            }
+            if (Input.GetKey(KeyCode.W))
+            {
+                v = 1;
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                v = -1;
+            }
+        }
 
         // AD 키로 좌우 방향 전환
         transform.Rotate(Vector3.up * h * turnSpeed * Time.deltaTime);
@@ -36,5 +82,54 @@ public class CharacterMove : MonoBehaviour
         yVelocity += (gravity * Time.deltaTime);
         moveDirection.y = yVelocity;
         characterController.Move(moveDirection * Time.deltaTime);
+        if (playerNumber == 1 || playerNumber == 2)
+        {
+            keyText.text = "Key Input: " + GetKeyInput(); // GetKeyInput 함수를 호출하여 입력된 키를 표시합니다.
+        }
+    }
+    string GetKeyInput()
+    {
+        string inputText = playerPrefix + playerNumber + " ";
+
+        if (playerNumber == 2)
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                inputText += "<-, ";
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                inputText += "->, ";
+            }
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                inputText += "/\\, ";
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                inputText += "\\/, ";
+            }
+        }
+        else if (playerNumber == 1)
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                inputText += "A, ";
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                inputText += "D, ";
+            }
+            if (Input.GetKey(KeyCode.W))
+            {
+                inputText += "W, ";
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                inputText += "S, ";
+            }
+        }
+
+        return inputText.TrimEnd(' ', ','); // 끝의 쉼표 및 공백을 제거하여 표시합니다.
     }
 }
